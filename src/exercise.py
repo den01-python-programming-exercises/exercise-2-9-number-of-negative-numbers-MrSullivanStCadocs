e9fce9d01f068e269eec3a498c27d760e175a09c
MrSullivan <>
2021-04-20 21:14:42 +0000

    alot

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,17 @@
 def main():
     #write your code below this line
+    countOfNegativeNumbers = 0
+
+    while True:
+      number = int(input("Give a number: "))
+
+      if (number == 0):
+        break
+
+      if (number<0):
+        countOfNegativeNumbers = countOfNegativeNumbers + 1
+
+    print("Number of negative numbers: " + str(countOfNegativeNumbers))
 
 if __name__ == '__main__':
     main()
